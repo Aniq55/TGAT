cbc7e397b3334177bd93d699731fcd4e30a38133
Aniq55 <aniqrah@gmail.com>
2024-06-07 14:41:24 +0100

    bash file updated

--- run_all.sh
+++ run_all.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 # datasets=("CanParl" "Flights" "mooc" "SocialEvo" "UNtrade" "USLegis" "Contacts" "enron" "lastfm" "reddit" "uci" "UNvote" "wikipedia")
-# datasets=( "wikipedia" "reddit" "uci" )
-# datasets=("enron" "mooc" "Contacts")
-datasets=("uci")
+datasets=( "wikipedia" "reddit" "uci" )
 S=10  
 
 for dataset in "${datasets[@]}" 
 do
     echo "Running TGAT on dataset: $dataset"
     # Real data
-    for sample in {1..5}; do
+    for sample in {1..10}; do
         python -u learn_edge.py -d "$dataset" --bs 200 --uniform  --n_degree 20 --agg_method attn --attn_mode prod --gpu 0 --n_head 2 --prefix hello_world
     done
 
     # distorted data: all samples
-    for sample in {1..5}; do
+    for sample in {1..10}; do
         distort="intense_5_${sample}_"
         python -u learn_edge.py -d "$dataset" --distortion "$distort" --bs 200 --uniform  --n_degree 20 --agg_method attn --attn_mode prod --gpu 0 --n_head 2 --prefix hello_world
     done
 
-    for sample in {1..5}; do
+    for sample in {1..10}; do
         distort="shuffle_${sample}_"
         python -u learn_edge.py -d "$dataset" --distortion "$distort" --bs 200 --uniform  --n_degree 20 --agg_method attn --attn_mode prod --gpu 0 --n_head 2 --prefix hello_world
     done
